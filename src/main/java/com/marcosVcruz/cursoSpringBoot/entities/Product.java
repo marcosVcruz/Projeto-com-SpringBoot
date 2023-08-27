47019978eb82f6ccc10d832089c9f11573e4da53
Marcos Vinicius <marcos.viniciuscruz00@gmail.com>
2023-08-27 15:09:24 -0300

    associacao muitos para muitos com JoinTable

--- src/main/java/com/marcosVcruz/cursoSpringBoot/entities/Product.java
+++ src/main/java/com/marcosVcruz/cursoSpringBoot/entities/Product.java
@@ -14,13 +14,13 @@ public class Product implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private String name;
     private String description;
     private Double price;
     private String imgUrl;
 
-    @Transient
+    @ManyToMany
+    @JoinTable(name = "tb_product_category", joinColumns = @JoinColumn(name = "product_id"), inverseJoinColumns = @JoinColumn(name = "category_id"))
     private Set<Category> categories = new HashSet<>();
 
     public Product() {
